654829c73a698bf3d33f521ed13234d7d0e13c55
nkhkcat <7u6a6i@gmail.com>
2024-01-13 18:15:33 +0900

    必要なページとデータのファイルを追加した

---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+
+get '/memos' do
+  file = File.read('data.json')
+  @data = JSON.parse(file)
+  erb :memos
+end
+
+post '/memos' do
+# TODO：新規作成処理
+end
+
+get '/memos/create' do
+  erb :create
+end
+
+get '/memos/:id' do
+  @id = params[:id]
+
+  erb :detail
+end
+
+patch '/memos/:id' do
+  # TODO:更新処理
+end
+
+get '/memos/:id/edit' do
+  erb :edit
+end
+
+not_found do
+  status 404
+  erb :not_found
+end
