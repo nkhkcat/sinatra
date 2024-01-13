c51f9bd4f99c373716555ac041401cc83b063a85
nkhkcat <7u6a6i@gmail.com>
2024-01-13 22:57:50 +0900

    更新処理を追加した

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/contrib'
 require 'json'
 
 get '/memos' do
@@ -47,10 +48,26 @@ get '/memos/:id' do
 end
 
 patch '/memos/:id' do
-  # TODO: 更新処理
+  file = File.read('data.json')
+  data = JSON.parse(file)
+
+  id = params[:id]
+  memo = data['memos'].find { |m| m['id'] == id }
+  edited_memo = { 'title' => params[:title], 'content' => params[:content] }
+  memo.merge!(edited_memo)
+
+  File.open('data.json', 'w') do |f|
+    f.write(JSON.pretty_generate(data))
+  end
+
+  redirect '/memos'
 end
 
 get '/memos/:id/edit' do
+  file = File.read('data.json')
+  @data = JSON.parse(file)
+  @id = params[:id]
+
   erb :edit
 end
 
